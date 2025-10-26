6acd9b59d1de9cecfb8052bb7847a91a66f1243c
Karim BOUALAM <boualam.karim94@gmail.com>
2025-10-26 12:40:59 +0100

    Initial commit - import Spring Boot base project

---
+++ src/main/java/com/atelier/tennis/TennisStatsApiApplication.java
@@ -0,0 +1,13 @@
+package com.atelier.tennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisStatsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisStatsApiApplication.class, args);
+	}
+
+}
